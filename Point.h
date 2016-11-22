022117d35fc2935235d9610cdf3c4c2ca20209d4
Hali <halivelazquez@outlook.com>
2016-11-22 10:06:00 -0600

    New 	modified:   .gitignore 	deleted:    Point.cpp 	modified:   Point.h

--- Point.h
+++ Point.h
@@ -7,19 +7,34 @@ class Point{
 		double x, y, z;
 		double eDist;
 	public:
-		Point(double = 0);
-		Point(double, double);
-		Point(double, double, double);
+		Point(double x1 = 0):
+			dim(1), x(x1), y(0), z(0), eDist(sqrt(x1 * x1)) {}
+		Point(double x1, double y1):
+			dim(2), x(x1), y(y1), z(0), eDist(sqrt(x1 * x1 + y1 * y1)) {}
+		Point(double x1, double y1, double z1):
+			dim(3), x(x1), y(y1), z(z1), eDist(sqrt(x1 * x1 + y1 * y1 + z1 * z1)) {}
 
-		void printPoint();
-		void setDim(int = 1);
-		void setPoint(double = 0, double = 0, double = 0);
-		double getD();
-		double getX();
-		double getY();
-		double getZ();
+		void printPoint() {
+			if (dim == 1) printf("(%.3lf)\n", x);
+			else if (dim == 2) printf("(%.3lf, %.3lf)\n", x, y);
+			else printf("(%.3lf, %.3lf, %.3lf)\n", x, y, z);
+		}
 
-		double distance(const Point);
+		void setDim(int d = 1) { dim = d; }
+
+		void setPoint(double x1 = 0, double y1 = 0, double z1 = 0) { x = x1, y = y1, z = z1; }
+
+		double getD() { return eDist; }
+
+		double getX() { return x; }
+
+		double getY() { return y; }
+
+		double getZ() { return z; }
+
+		double distance(const Point p) {
+			return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2) + pow(z - p.z, 2));
+		}
 };
 
 #endif

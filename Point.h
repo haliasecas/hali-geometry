3f9ade599956b1e37b40a61fd34645c85689e32b
Hali Velazquez <halivelazquez@outlook.com>
2017-12-28 01:15:11 -0600

    Delete getters and setters  Cambios a ser confirmados: 	modificado:     Line.h 	modificado:     Point.h

--- Point.h
+++ Point.h
@@ -2,61 +2,48 @@
 #define POINT_H
 
 class Point {
-	private:
-		int dim;
-		double x, y, z;
-		double eDist;
+  public:
+    int dimension;
+    double x, y, z;
+    double zeroDistance;
 
-	public:
-		Point(double x1 = 0):
-			dim(1), x(x1), y(0), z(0), eDist(sqrt(x1 * x1)) {}
+    Point(double x1 = 0):
+      dimension(1), x(x1), y(0), z(0), zeroDistance(sqrt(x1 * x1)) {}
 
-		Point(double x1, double y1):
-			dim(2), x(x1), y(y1), z(0),
-			eDist(sqrt(x1 * x1 + y1 * y1)) {}
+    Point(double x1, double y1):
+      dimension(2), x(x1), y(y1), z(0),
+      zeroDistance(sqrt(x1 * x1 + y1 * y1)) {}
 
-		Point(double x1, double y1, double z1):
-			dim(3), x(x1), y(y1), z(z1),
-			eDist(sqrt(x1 * x1 + y1 * y1 + z1 * z1)) {}
+    Point(double x1, double y1, double z1):
+      dimension(3), x(x1), y(y1), z(z1),
+      zeroDistance(sqrt(x1 * x1 + y1 * y1 + z1 * z1)) {}
 
-		void setDim(int d = 1) { dim = d; }
+    void setPoint(double x1 = 0, double y1 = 0, double z1 = 0) {
+      x = x1, y = y1, z = z1;
+      if (x1 > 0) dimension = 1;
+      if (y1 > 0) dimension = 2;
+      if (z1 > 0) dimension = 3;
+    }
 
-		void setPoint(double x1 = 0, double y1 = 0, double z1 = 0) {
-			x = x1, y = y1, z = z1;
-		}
+    double distance(const Point &p) {
+      return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2) + pow(z - p.z, 2));
+    }
 
-		double getD() { return eDist; }
-
-		double getX() { return x; }
-
-		double getY() { return y; }
-
-		double getZ() { return z; }
-
-		int getDim() { return dim; }
-
-		double distance(const Point &p) {
-			return sqrt(pow(x - p.x, 2) + pow(y - p.y, 2) + pow(z - p.z, 2));
-		}
-
-		bool operator<(const Point &p) {
-			return p.eDist < this->eDist;
-		}
+    bool operator<(const Point &p) {
+      return p.zeroDistance < this->zeroDistance;
+    }
 
 };
 
 std::ostream &operator<<(std::ostream &out, Point &p) {
-	std::streamsize oldp = out.precision();
-	out << std::fixed << std::setprecision(3);
-	int dim = p.getDim();
-	out << '(';
-	if (dim == 1) out << p.getX();
-	else if (dim == 2) out << p.getX() << ',' << p.getY();
-	else out << p.getX() << ',' << p.getY() << ',' << p.getZ();
-	out << ')';
-	out << std::setprecision(oldp);
+  int dimension = p.dimension;
+  out << '(';
+  if (dimension == 1) out << p.x;
+  else if (dimension == 2) out << p.x << ',' << p.y;
+  else out << p.x << ',' << p.y << ',' << p.z;
+  out << ')';
 
-	return out;
+  return out;
 }
 
 #endif
